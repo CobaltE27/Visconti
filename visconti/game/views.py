3c6ce6aa5b50753388a806bdad2e2cc52a967b29
CobaltE27 <potassiume19@gmail.com>
2025-05-17 22:44:17 -0600

    reverted debug starting lots

--- visconti/game/views.py
+++ visconti/game/views.py
@@ -49,8 +49,8 @@ def set_name(request):
     if request.method == "POST":
         newName = request.POST["name"]
         if not models.get_players().filter(name=newName).exists():
-            # newPlayer = models.Player.objects.create(name=newName, current_bid=0)
-            newPlayer = models.Player.objects.create(name=newName, current_bid=0, lots="G10 g1 c2 d3 s4 f5")
+            newPlayer = models.Player.objects.create(name=newName, current_bid=0)
+            # newPlayer = models.Player.objects.create(name=newName, current_bid=0, lots="G10 g1 c2 d3 s4 f5")
             newPlayer.save()
             models.advance_step()
             return HttpResponse()
