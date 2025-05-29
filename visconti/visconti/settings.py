5a069d0f80e8c0fea291af0b2c3428fc13e90477
CobaltE27 <potassiume19@gmail.com>
2025-05-29 16:31:29 -0600

    no more CORS override

--- visconti/visconti/settings.py
+++ visconti/visconti/settings.py
@@ -127,10 +127,10 @@ STATICFILES_DIRS = [ 'static/' ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+# ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
